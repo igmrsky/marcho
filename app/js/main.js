b72af83176efd543055b96f30bdd7c7a5c240a0c
igmrsky <mrinskiy0282@mail.ru>
2024-03-24 22:53:28 +0400

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,16 @@
-$(function(){
+$(function () {
     $('.top-slider__inner').slick({
-        dots:true,
+        dots: true,
         arrows: false,
         fade: true,
         autoplay: true,
         autoplaySpeed: 3000
     })
+
+    $('.star').rateYo({
+        starWidth: '17px',
+        normalFill: '#ccccce',
+        ratedFill: '#ffc35b',
+        readOnly: true
+    });
 });
